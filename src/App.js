313a64763d2cbd55b256b528a2cfc6b9d7f6f572
sethespinoza <seth.espinoza8@gmail.com>
2025-07-01 22:12:42 -0500

    Create chat layout structure w/ users, messages, and input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="chat-container">
+      <aside className="sidebar">Users</aside>
+      <main className="chat-main">
+        <div className="messages">Hello!</div>
+        <div className="new-message">
+          <input type="text" placeholder="type a message..." />
+          </div>
+      </main>
     </div>
+      
+  
   );
 }
 
